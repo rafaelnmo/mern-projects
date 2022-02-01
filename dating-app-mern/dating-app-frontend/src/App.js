3a6d5db80bf0665a6f2be5d0fea6e70e0d149d88
rafaelnmo <rafaelnevesmello@gmail.com>
2022-02-01 12:34:16 -0300

    Icluding on App.js react components created

--- dating-app-mern/dating-app-frontend/src/App.js
+++ dating-app-mern/dating-app-frontend/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Header from './components/Header';
+import DatingCards from './components/DatingCards';
+import SwipeButtons from './components/SwipeButtons';
 
 function App() {
   return (
     <div className="App">
       <h1>Dating App MERN</h1>
+      <Header/>
+      <DatingCards/>
+      <SwipeButtons/>
     </div>
   );
 }
